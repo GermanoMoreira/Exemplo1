e0f2fb4e44d1b84a479d0b0a472575563f788705
GermanoMoreira <germanus17@gmail.com>
2015-11-07 16:34:45 -0300

    Initial commit

---
+++ app/src/main/java/corp/migu/de/show/exemplo1/MainQuickCare.java
@@ -0,0 +1,13 @@
+package corp.migu.de.show.exemplo1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainQuickCare extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_quick_care);
+    }
+}
